c661710fe5b661c02be198e1d76f1b6fa8bc0abd
Cameron Luo <cameronluo@gatech.edu>
2022-05-16 20:34:22 +0800

    Add logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,15 +3,24 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-
+import { ReactSVG } from "react-svg";
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar color="primary" position="static">
         <Toolbar>
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-            <u>S</u>
-          </Typography>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{ flexGrow: 0 }}
+          ></Typography>
+          <ReactSVG
+            left
+            src="stackline_logo.svg"
+            width="0"
+            height="0"
+            style={{ width: 100 }}
+          />
         </Toolbar>
       </AppBar>
     </Box>
